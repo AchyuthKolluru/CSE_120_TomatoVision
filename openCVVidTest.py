ff9103f7328f10c7a819ac7dfcc137ef61eb7686
Brandon <brandonkieng182@gmail.com>
2022-11-18 17:20:46 -0800

    added pop up for in video feed test file

--- openCVVidTest.py
+++ openCVVidTest.py
@@ -1,10 +1,16 @@
 import numpy as np
 import cv2 as cv
+
+toggle = False
+chuc = True
+
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+count = 0
 while True:
+    count = count + 1;
     # Capture frame-by-frame
     ret, frame = cap.read()
     # if frame is read correctly ret is True
@@ -12,12 +18,18 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.rectangle(gray,(384,0),(510,128),(0,255,0),3)
+    if count == 100:
+        toggle = True
+    if toggle:
+        cv.rectangle(frame,(0,0),(720,720),(0,0,255),-1)
     # Display the resulting frame
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        breakq
+    cv.putText(frame, "frame: {}".format(count), (0, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (0,0,0), 3)
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord(' '):
+        toggle = False
+        chuc = False
+
+
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
