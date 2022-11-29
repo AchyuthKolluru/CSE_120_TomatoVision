851601da66619d53a44230cc04c136c453a507e3
Achyuth Kolluru <kolluruachyuth@gmail.com>
2022-11-28 18:04:32 -0800

    Finished creating a counter for getting the total count of tomatoes

---
+++ tomatoCounting/[vehicles_counting]3_object_tracking.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from object_tracker import VehiclesCounting
+import cv2
+
+video = 'data/videos/video2.mp4' # Video's path
+log_filename = 'Camera 1' # log_filename
+line_position = 0.05 # location of line caculated by height of image
+line_angle = 0
+
+# initialize
+VC = VehiclesCounting(log_filename, video=video, info=False,
+                      dont_show=False,
+                      output='./result1.mp4',
+                      detection_line=(line_position, line_angle))
+
+# render video
+VC.run()
